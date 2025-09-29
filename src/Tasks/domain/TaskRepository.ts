7333320c174b32dc951dc4857af4fd35c6191d9a
ojeelafriend <ts8irolon@gmail.com>
2025-09-29 19:33:13 -0300

    [modify] change any returns

--- src/Tasks/domain/TaskRepository.ts
+++ src/Tasks/domain/TaskRepository.ts
@@ -1,8 +1,12 @@
-import { type Task } from "./Task";
+import { TaskStatus, type Task } from "./Task";
 
 export interface TaskRepository {
   save(task: Task): Promise<Task>;
-  updateStatusById({ id }: { id: string }): Promise<void>;
+  updateStatusById({
+    id,
+  }: {
+    id: string;
+  }): Promise<{ id: string; status: TaskStatus }>;
   deleteById({ id }: { id: string }): Promise<void>;
   findAll(): Promise<Task[]>;
   findById({ id }: { id: string }): Promise<Task>;
