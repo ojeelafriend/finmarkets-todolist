12b4c406eb8ed936e1275a36a6e306268e59bce0
ojeelafriend <ts8irolon@gmail.com>
2025-09-29 19:32:47 -0300

    [modify] implement CRUD using repository and use cases (application and framework)

--- src/Api/routes/task.routes.ts
+++ src/Api/routes/task.routes.ts
@@ -1,18 +1,24 @@
 import { Router, Request, Response } from "express";
 import { io } from "../server";
 
-import {
-  validateCreatedTask,
-  validateUpdatedTaskStatus,
-} from "./middlewares/validators/task";
+import { validateCreatedTask } from "./middlewares/validators/task";
 
 import { SQLiteTaskRepository } from "../../Tasks/framework/Database/SQLiteTaskRepository";
 import { Creator } from "../../Tasks/application/Creator";
+import { Updater } from "../../Tasks/application/Updater";
+import { FinderAll } from "../../Tasks/application/Finder/FinderAll";
+import { FinderById } from "../../Tasks/application/Finder/FinderById";
+import { Deleter } from "../../Tasks/application/Deleter";
 
 const router = Router();
 
 const taskRepository = new SQLiteTaskRepository();
+
 const creator = new Creator(taskRepository);
+const updater = new Updater(taskRepository);
+const finderAll = new FinderAll(taskRepository);
+const finderById = new FinderById(taskRepository);
+const deleter = new Deleter(taskRepository);
 
 router.post(
   "",
@@ -37,46 +43,83 @@ router.post(
       return response.status(201).json({ ok: true, task });
     } catch (error) {
       console.log(error);
-      response
-        .status(500)
-        .json({ ok: false, message: "Internal server error" });
+      response.status(500).json({ ok: false, error: "Internal server error" });
     }
   }
 );
 
-router.patch(
-  "/status/:id",
-  validateUpdatedTaskStatus,
-  (request: Request, response: Response) => {
-    try {
-    } catch (error) {
-      console.log(error);
-      response.status(500).json({ message: "Internal server error" });
+router.patch("/:id", async (request: Request, response: Response) => {
+  try {
+    const { id } = request.params;
+
+    const { taskUpdatedInfo, error } = await updater.execute({ id });
+
+    if (error) {
+      return response.status(400).json({
+        ok: false,
+        error,
+      });
     }
-  }
-);
 
-router.get("", (request: Request, response: Response) => {
-  try {
+    io.emit("taskUpdated", taskUpdatedInfo);
+
+    return response.status(200).json({ ok: true, task: taskUpdatedInfo });
   } catch (error) {
-    console.log(error);
-    response.status(500).json({ message: "Internal server error" });
+    response.status(500).json({ ok: false, error: "Internal server error" });
   }
 });
 
-router.get("/:id", (request: Request, response: Response) => {
+router.get("", async (request: Request, response: Response) => {
   try {
+    const { tasks, error } = await finderAll.execute();
+
+    if (error) {
+      return response.status(400).json({
+        ok: false,
+        error,
+      });
+    }
+
+    return response.status(200).json({ ok: true, tasks });
   } catch (error) {
     console.log(error);
-    response.status(500).json({ message: "Internal server error" });
+    response.status(500).json({ ok: false, error: "Internal server error" });
   }
 });
 
-router.delete("/:id", (request: Request, response: Response) => {
+router.get("/:id", async (request: Request, response: Response) => {
   try {
+    const { id } = request.params;
+
+    const { task, error } = await finderById.execute({ id });
+
+    if (error) {
+      return response.status(400).json({ ok: false, error });
+    }
+
+    return response.status(200).json({ ok: true, task });
   } catch (error) {
     console.log(error);
-    response.status(500).json({ message: "Internal server error" });
+    response.status(500).json({ ok: false, error: "Internal server error" });
+  }
+});
+
+router.delete("/:id", async (request: Request, response: Response) => {
+  try {
+    const { id } = request.params;
+
+    const { taskDeletedInfo, error } = await deleter.execute({ id });
+
+    if (error) {
+      return response.status(400).json({ ok: false, error });
+    }
+
+    io.emit("taskDeleted", taskDeletedInfo);
+
+    return response.status(200).json({ ok: true, task: taskDeletedInfo });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ ok: false, error: "Internal server error" });
   }
 });
 
