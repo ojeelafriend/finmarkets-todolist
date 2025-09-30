cb7b62e448ec0c35f48bec7bb08903f6cc05b68d
ojeelafriend <ts8irolon@gmail.com>
2025-09-30 09:36:09 -0300

    setting with deploy: add /health, transpiling improve (tsconfig), move dev dependencies to essential dependencies

--- src/Api/express.ts
+++ src/Api/express.ts
@@ -6,7 +6,12 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const app = express();
 
 app.use(express.json(), express.urlencoded({ extended: false }));
+
 app.use(morgan("dev"));
+
+app.get("/health", (_, res) => {
+  res.json({ ok: true, message: "Server is running" });
+});
 
 app.use(cors({ origin: CLIENT_URL || "*" }));
 
