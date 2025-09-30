63567be6936d9220f10d109689f3cfbdeea4a911
ojeelafriend <ts8irolon@gmail.com>
2025-09-30 14:22:37 -0300

    [modify] prepare database for testing

--- src/Tasks/framework/Database/TypeORM/config.ts
+++ src/Tasks/framework/Database/TypeORM/config.ts
@@ -1,10 +1,22 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Task } from "./TaskEntity";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const database =
+  process.env.NODE_ENV === "production"
+    ? "data/tasks.sqlite"
+    : process.env.NODE_ENV === "development"
+    ? "tasks.sqlite"
+    : process.env.NODE_ENV === "test"
+    ? "test.sqlite"
+    : "tasks.sqlite";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
-  database: "data/tasks.sqlite",
+  database,
   entities: [Task],
   synchronize: true,
   logging: false,
